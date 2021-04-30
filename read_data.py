a0714cd2f859dcb9903f333b1cfb379043d95075
Zach Judson <zjudson@protonmail.com>
2021-04-30 11:57:05 -0700

    Added test/train splitting

--- read_data.py
+++ read_data.py
@@ -7,19 +7,24 @@ with open("data.txt") as file:
 
 database = ast.literal_eval(database)
 
-# Build word list from data
-words = []
-for item in database['data']:
-    for key in item.keys():
-        if key == '_id':
-            continue
-        else:
-            for word in item[key]:
-                if word not in words:
-                    words.append(word)
-words.sort()
+def build_word_list():
+    # Build word list from data
+    words = []
+    queries = 0
+    for item in database['data']:
+        for key in item.keys():
+            if key == '_id':
+                queries+=1
+            else:
+                for word in item[key]:
+                    if word not in words:
+                        words.append(word)
+    words.sort()
+    
+    print("Queries:", queries)
+    print("Items:", len(database['data']))
+    # Export to pickle
+    with open("wordlist.pkl", "wb") as file:
+        pickle.dump(words, file)
 
-# Export to pickle
-with open("wordlist.pkl", "wb") as file:
-    pickle.dump(words, file)
-
+build_word_list()
