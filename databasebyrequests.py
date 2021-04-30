4a188a48892ee00e15b1ee8b148675544ce1dce7
your_username <vkdinh@ucsc.edu>
2021-04-30 11:12:20 -0700

    Update databasebyrequests.py

--- databasebyrequests.py
+++ databasebyrequests.py
@@ -1,5 +1,19 @@
-  
+import os
+import sys
 import requests
 
+import numpy
+import ast
+from sklearn.feature_extraction.text import TfidVectorizer
+
 r = requests.get('https://t4g-dl-api.herokuapp.com/api/datasets/proj2/')
-print(r.text)
+#sys.stdout = open("data.txt", "w")
+#print(r.text)
+#sys.stdout.close()
+
+data = ast.literal_eval(r.text)
+print("Question ID: ", data['data'][1]['_id'])
+print("Title words: ", data['data'][1]['title'])
+print("Query words: ", data['data'][1]['query'])
+print("Indicated label: ", data['data'][1]['label'])
+
