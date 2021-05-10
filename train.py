b723e6d9041f6c8ee3421181e25c213edc808e17
Zach Judson <zjudson@protonmail.com>
2021-05-10 12:13:56 -0700

    Added pickle weights

--- train.py
+++ train.py
@@ -10,6 +10,9 @@ from read_data import *
 # database = ast.literal_eval(database)
 # test_data, train_data = split_data(database)
 
+with open("train_data.pkl", "rb") as file:
+    train_data = pickle.load(file)
+
 with open("test_data.pkl", "rb") as file:
     test_data = pickle.load(file)
 
@@ -22,14 +25,13 @@ with open("wordlist.pkl", "rb") as file:
 with open("weights.pkl", "rb") as file:
     weights = pickle.load(file)
 
-weights = np.ones(weights.shape)
 # test_data, train_data = split_data
 # wordlist = build_word_list(train_data)
 # categories = build_category_vecs(wordlist, train_data)
+training = gradient_descent(train_data, categories, wordlist, .01, 3)
 
 test_model(weights, test_data, categories, wordlist)
 
-# training = gradient_descent(train_data, categories, wordlist, .1, 30)
 
-# Jwith open("weights.pkl", "wb") as file:
+# with open("weights.pkl", "wb") as file:
 #     pickle.dump(training[0], file)
